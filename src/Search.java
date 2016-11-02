e0b32b5a256abdc88fcd6989869fa47e6b105d58
Crow404 <YulyaLi404@gmail.com>
2016-11-02 18:30:04 +0300

    Update Search.java

--- src/Search.java
+++ src/Search.java
@@ -11,7 +11,6 @@ import java.util.ArrayList;
  */
 public class Search extends ArrayUtils{
 
-
     public static void main(String[] args) throws IOException {
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
