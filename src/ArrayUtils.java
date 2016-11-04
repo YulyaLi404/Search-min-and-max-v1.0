fc013be4cb08b9f81306f9920f05001ef9b27a5f
Crow404 <YulyaLi404@gmail.com>
2016-11-04 19:56:22 +0300

    Update ArrayUtils.java

--- src/ArrayUtils.java
+++ src/ArrayUtils.java
@@ -39,7 +39,7 @@ class ArrayUtils {
 
        int[] num = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9 , 10};
 
-       for (int i = num.length ; i > 0; i--) {
+       for (int i = num.length; i > 0; i--) {
            System.out.println(i);
        }
        return num;
