39699216866e2e6af775e3b798655c19f1423e4f
Crow404 <YulyaLi404@gmail.com>
2016-11-06 11:53:51 +0300

    Update Search.java

--- src/Search.java
+++ src/Search.java
@@ -22,7 +22,8 @@ public class Search {
         System.out.println("Search for the min and max numbers.\n" + line);
         
         try {
-
+            System.out.println("Enter numbers: ");
+            
             for (int i = 0; i < 100; i++) {
 
                 array.add(Integer.parseInt(reader.readLine()));
@@ -36,13 +37,28 @@ public class Search {
         System.out.println(line + "\nMaximum: "+ maximum);
         System.out.println("Minimum: " + minimum);
         
-        System.out.println(Arrays.toString(ArrayUtils.revers()));
+        System.out.println(line + "\n" + "The output of the array in reverse order.");
+        int[] arr = {1, 4, 0, 5, 4, 0, 5, 4};
+        arr = ArrayUtils.reverse(arr);
+
+        System.out.println(line + "\n" + Arrays.toString(arr));
+
+        System.out.println(line + "\n" + "The sum of the two numbers.");
+        System.out.println("Enter 2 numbers: ");
+
+        int a = Integer.parseInt(reader.readLine());
+        int b = Integer.parseInt(reader.readLine());
+
+        // Получаем сумму двух переменных
+        int sum = ArrayUtils.addition(a, b);
+        System.out.println(line + "\nSum: " + sum);
     }
 }
 
 /* Output:
 Search for the min and max numbers.
 -----------------------------------
+Enter numbers:
 45666
 34555
 2334
@@ -53,17 +69,16 @@ Search for the min and max numbers.
 -----------------------------------
 Maximum: 45666
 Minimum: -2334
-
-10
-9
-8
-7
-6
-5
-4
-3
-2
-1
-[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+-----------------------------------
+Output of the array in reverse order.
+[4, 5, 0, 4, 5, 0, 4, 1]
+-----------------------------------
+The sum of the two numbers.
+-----------------------------------
+Enter 2 numbers: 
+-1
+55
+-----------------------------------
+Sum: 54
 
 */
